cbaa5f8e04eae130244ede26a68d1e7c79e22f37
Said Molina <68343481+said-molina@users.noreply.github.com>
2023-08-28 22:30:42 -0600

    inserdatos

---
+++ insertdatos.sql
@@ -0,0 +1,27 @@
+insert into "users"  (username, useremail, password, age) values 
+('said', 'molina@gmail', '123', 32)
+
+insert into "users"  (username, useremail, password, age) values 
+('carlos', 'guevara@gmail', '123', 22)
+
+insert into "courses"  (title, description , level ) values 
+('Matematicas', 'matematicas de primer grado', 'avanzado')
+
+insert into "courses"  (title, description , level ) values 
+('Fisica', 'Fisica de primer teoria', 'intermedio')
+
+insert into "coursevideo"  (course_title, course_url ) values 
+('Fisica', 'www.youtube/fisica.com')
+
+insert into "coursevideo"  (course_title, course_url ) values 
+('Matematicas', 'www.youtube/matematicas.com')
+
+insert into "categories"  ( categorie_name ) values 
+('ciencias')
+
+insert into "categories"  ( categorie_name ) values 
+('computacion')
+
+insert into "roles"  ( roles_name ) values 
+('teacher'),('student'),('admin')
+
